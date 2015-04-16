d652a1cb2e30384ea9acae06020c2776290c4d5b
Stephanie Lauziere <salauzie@gmail.com>
2015-04-15 23:43:34 -0400

    attempt to fix footer

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :teachers
   resources :about
+  resources :gigs
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
